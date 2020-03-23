731226228adb4cb8f9742546e30b1f30b039168d
Jovan Shandro <shandrojovan@gmail.com>
2020-03-23 19:56:08 +0100

    Refactor code, make transition horizontal, add link to new page

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  CardStyleInterpolators
+} from "@react-navigation/stack";
 import Home from "./screens/Home";
 import Edit from "./screens/Edit";
 import Show from "./screens/Show";
@@ -11,34 +14,43 @@ import IconTitle from "./components/IconTitle";
 import { createStore } from "redux";
 import allReducers from "./store/reducers";
 import { Provider } from "react-redux";
+import { AntDesign } from "@expo/vector-icons";
 
 const store = createStore(allReducers);
 
 const Stack = createStackNavigator();
 
-const options = {
-  headerTitle: props => <IconTitle {...props} />,
-  headerStyle: {
-    backgroundColor: "#1b1c1d"
-  },
-  headerTintColor: "#fff",
-  headerTitleAlign: "center"
-};
-
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Show">
-          <Stack.Screen name="Home" component={Home} options={options} />
-          <Stack.Screen name="New" component={New} options={options} />
-          <Stack.Screen name="Edit" component={Edit} options={options} />
-          <Stack.Screen name="Show" component={Show} options={options} />
-          <Stack.Screen
-            name="ShowSingle"
-            component={ShowSingle}
-            options={options}
-          />
+        <Stack.Navigator
+          initialRouteName="Show"
+          screenOptions={({ navigation }) => ({
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+            headerTitle: props => <IconTitle {...props} />,
+            headerStyle: {
+              backgroundColor: "#1b1c1d"
+            },
+            headerTintColor: "#fff",
+            headerTitleAlign: "center",
+            headerRight: () => (
+              <View style={{ marginRight: 15 }}>
+                <AntDesign
+                  onPress={() => navigation.navigate("New")}
+                  name="plus"
+                  size={25}
+                  color="#fff"
+                />
+              </View>
+            )
+          })}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="New" component={New} />
+          <Stack.Screen name="Edit" component={Edit} />
+          <Stack.Screen name="Show" component={Show} />
+          <Stack.Screen name="ShowSingle" component={ShowSingle} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
