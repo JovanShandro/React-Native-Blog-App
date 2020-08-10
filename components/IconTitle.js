17ecfd7e41181273d75cae8d915172c70a3ff98d
Jovan Shandro <shandrojovan@gmail.com>
2020-08-10 11:21:29 +0200

    Small clean up for icon title component

--- components/IconTitle.js
+++ components/IconTitle.js
@@ -2,14 +2,12 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
-const IconTitle = () => {
-  return (
-    <View style={styles.container}>
-      <Entypo style={{ ...styles.iconAndText, ...styles.icon }} name="code" />
-      <Text style={{ ...styles.iconAndText, ...styles.text }}>Blog App</Text>
-    </View>
-  );
-};
+const IconTitle = () => (
+  <View style={styles.container}>
+    <Entypo style={{ ...styles.iconAndText, ...styles.icon }} name="code" />
+    <Text style={{ ...styles.iconAndText, ...styles.text }}>Blog App</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
